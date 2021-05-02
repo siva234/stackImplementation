b927604f29553c3d39db64f560908a832c388842
siva reddy <siva.agent@gmail.com>
2021-05-02 21:15:20 +0200

    stack implementation done

--- stack.js
+++ stack.js
@@ -1,1 +1,40 @@
-// Your implementation goes here
+module.exports = class Stack {
+  constructor(...args) {
+    this.data = args;
+    if (this.data.length == 0) this.top = -1;
+    else this.top = this.data.length - 1;
+  }
+
+  push(element) {
+    this.data.push(element);
+    this.top = this.top + 1;
+  }
+
+  peek() {
+    return this.data[this.top];
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  pop() {
+    if (this.isEmpty() === false) {
+      this.top = this.top - 1;
+      return this.data.pop();
+    }
+    return null;
+  }
+
+  swap() {
+    if (this.data.length > 1) {
+      let tmp = this.data[this.top];
+      this.data[this.top] = this.data[this.top - 1];
+      this.data[this.top - 1] = tmp;
+    }
+  }
+
+  get size() {
+    return this.data.length;
+  }
+};
